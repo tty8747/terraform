60a71f6307755fcce9b7eee21b7d152796807dbf
Sergey Chulanov <tty8747@gmail.com>
2018-11-26 15:46:58 +0300

    Add sec groups

--- inst.tf
+++ inst.tf
@@ -1,12 +1,67 @@
 resource "aws_instance" "front" {
-  ami           = "${var.ami}"
-  instance_type = "${var.insttype}"
-  key_name = "${aws_key_pair.sshpubkey.id}"
+  ami                         = "${var.ami}"
+  instance_type               = "${var.insttype}"
+  key_name                    = "${aws_key_pair.sshpubkey.id}"
   associate_public_ip_address = true
-  cpu_threads_per_core = "1"
-  security_groups = ["default"]
-
+  cpu_threads_per_core        = "1"
+  security_groups             = ["${aws_security_group.front.name}"]
   tags {
-    Name = "front"
+    Name                      = "front"
   }
+}
+
+resource "aws_instance" "api" {
+  ami                         = "${var.ami}"
+  instance_type               = "${var.insttype}"
+  key_name                    = "${aws_key_pair.sshpubkey.id}"
+  associate_public_ip_address = true
+  cpu_threads_per_core        = "1"
+  security_groups             = ["${aws_security_group.api.name}"]
+  tags {
+    Name                      = "api"
+  }
+}
+
+resource "aws_ebs_volume" "volume-front" {
+  count             = 1
+  availability_zone = "${aws_instance.front.availability_zone}"
+  type              = "gp2"
+  size              = 1
+}
+
+resource "aws_ebs_volume" "volume-api" {
+  count             = 1
+  availability_zone = "${aws_instance.api.availability_zone}"
+  type              = "gp2"
+  size              = 1
+}
+
+resource "aws_volume_attachment" "volume-attach-front" {
+  device_name = "/dev/xvda"
+  instance_id = "${aws_instance.front.id}"
+  volume_id   = "${aws_ebs_volume.volume-front.id}"
+}
+
+resource "aws_volume_attachment" "volume-attach-api" {
+  device_name = "/dev/xvdb"
+  instance_id = "${aws_instance.api.id}"
+  volume_id   = "${aws_ebs_volume.volume-api.id}"
+}
+
+resource "aws_security_group" "front" {
+  name        = "front"
+}
+
+resource "aws_security_group" "api" {
+  name        = "api"
+}
+
+resource "aws_security_group_rule" "ssh_access" {
+  count             = "${length(var.ssh_port)}"
+  type              = "ingress"
+  from_port         = "${element(var.ssh_port, count.index)}"
+  to_port           = "${element(var.ssh_port, count.index)}"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.front.id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,26 +1,31 @@
 variable "aws-region" {
-  default = "us-east-2"
+  default     = "us-east-2"
   description = "Default region"
 }
 
 variable "ami" {
-  default = "ami-0f65671a86f061fcd"
+  default     = "ami-0f65671a86f061fcd"
   description = "Region"
 }
 
 variable "insttype" {
-  default = "t2.micro"
+  default     = "t2.micro"
   description = "Type instance"
 }
 
 variable "domainname" {
-  type = "list"
-  default = ["ubukubu.ru.","aaaj.ru."]
+  type        = "list"
+  default     = ["ubukubu.ru.","aaaj.ru."]
 }
 
 variable "keypath" {
-  default = "~/.ssh/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
   description = "path to pub key"
+}
+
+variable "ssh_port" {
+  type        = "list"
+  default     = ["22"]
 }
 
 variable "vpc_cidr" {
